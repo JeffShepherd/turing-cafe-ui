995bd3dfaf02eb963fbec8a62f526f806514c73b
JS <jeff.shep777@gmail.com>
2021-04-07 10:11:16 -0600

    Style reservation card

--- src/Components/Reservation/Reservation.js
+++ src/Components/Reservation/Reservation.js
@@ -4,11 +4,11 @@ import './Reservation.css'
 const Reservation = ({ id, name, date, time, number}) => {
 
 return (
-  <article id={id}>
-    <p>{name}</p>
+  <article id={id} className="res-card">
+    <h3>{name}</h3>
     <p>{date}</p>
-    <p>{time}</p>
-    <p>{number}</p>
+    <p>{time} pm</p>
+    <p>Number of guests: {number}</p>
   </article>
 )
 
