ca739d41c6c19c336fdfd6d022c59af2477cdf4f
JS <jeff.shep777@gmail.com>
2021-04-07 10:47:03 -0600

    Create controlled form

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -12,14 +12,19 @@ class Form extends Component {
     }
   }
 
+  handleChange = (data, dataCategory) => {
+    this.setState({ [dataCategory]: data})
+    console.log(this.state)
+  }
+
 
 render() {
   return (
     <form>
-      <input placeholder="Name"></input>
-      <input placeholder="Date (mm/dd)"></input>
-      <input placeholder="Time"></input>
-      <input placeholder="Number of Guests"></input>
+      <input placeholder="Name" onChange={(event) => this.handleChange(event.target.value, 'name')}></input>
+      <input placeholder="Date (mm/dd)" onChange={(event) => this.handleChange(event.target.value, 'date')}></input>
+      <input placeholder="Time" onChange={(event) => this.handleChange(event.target.value, 'time')}></input>
+      <input placeholder="Number of Guests" onChange={(event) => this.handleChange(event.target.value, 'number')}></input>
       <button>Make Reservation</button>
     </form>
   )
