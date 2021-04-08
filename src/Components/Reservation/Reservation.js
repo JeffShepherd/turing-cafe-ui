32fcbd328b587f61ae7b1128fa69e5cd6a1d9e75
JS <jeff.shep777@gmail.com>
2021-04-07 19:57:48 -0600

    Add reservation cancel functionality

--- src/Components/Reservation/Reservation.js
+++ src/Components/Reservation/Reservation.js
@@ -1,14 +1,15 @@
 import React from 'react'
 import './Reservation.css'
 
-const Reservation = ({ id, name, date, time, number}) => {
+const Reservation = ({ id, name, date, time, number, deleteReservation}) => {
 
 return (
-  <article id={id} className="res-card">
+  <article className="res-card">
     <h3>{name}</h3>
     <p>{date}</p>
     <p>{time} pm</p>
     <p>Number of guests: {number}</p>
+    <button id={id} onClick={(event) => deleteReservation(event.target.id)}>Cancel</button>
   </article>
 )
 
